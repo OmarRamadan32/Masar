90cf21ce3c3b864934e595aadc7373ac4b9f525b
Omar <omarramadan021@gmail.com>
2026-09-01 18:19:12 +0300

    refactor: project folder structure is done

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:masar/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const Masar());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
