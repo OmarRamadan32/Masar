a956fc31dcac96fb3fbf2da3ca6ae1c348efc9ba
Omar <omarramadan021@gmail.com>
2026-09-02 20:38:49 +0300

    Added app fonts to pubspec.yaml folder

--- lib/core/theme/app_styles.dart
+++ lib/core/theme/app_styles.dart
@@ -1,9 +1,5 @@
 abstract class AppStyles {
 
-  static const double fontSize12 = 12;
-  static const double fontSize14 = 14;
-  static const double fontSize16 = 16;
-  static const double fontSize18 = 18;
-  static const double fontSize20 = 20;
+ 
   
 }
