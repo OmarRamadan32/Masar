9116cf95a07edf1cea46a77146b84e16a5c31a9e
Omar <omarramadan021@gmail.com>
2026-09-05 21:13:57 +0300

    refactor: simplify BottomNavBarItem layout and remove unused imports

--- lib/features/main_layout/widgets/bottom_nav_bar_item.dart
+++ lib/features/main_layout/widgets/bottom_nav_bar_item.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:masar/core/theme/app_colors.dart';
-import 'package:masar/core/theme/app_sizes.dart';
 import 'package:masar/core/theme/app_styles.dart';
 
 class BottomNavBarItem extends StatelessWidget {
@@ -23,44 +22,36 @@ class BottomNavBarItem extends StatelessWidget {
       padding: const EdgeInsets.symmetric(vertical: 5),
       child: GestureDetector(
         onTap: onTap,
-        child: AnimatedContainer(
-          duration: Duration(milliseconds: isActive ? 500 : 0),
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          decoration: BoxDecoration(
-            borderRadius: AppSizes.r16,
-            color: isActive ? AppColors.primaryContainer : Colors.transparent,
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              AnimatedSwitcher(
-                duration: const Duration(milliseconds: 500),
-                transitionBuilder: (Widget child, Animation<double> animation) {
-                  return ScaleTransition(
-                    scale: animation,
-                    child: ScaleTransition(scale: animation, child: child),
-                  );
-                },
-                child: Icon(
-                  icon,
-                  size: 24,
-                  key: ValueKey<bool>(isActive),
-                  color: isActive
-                      ? AppColors.primaryColor
-                      : AppColors.inactiveBottomNavItemColor,
-                ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedSwitcher(
+              duration: const Duration(milliseconds: 500),
+              transitionBuilder: (Widget child, Animation<double> animation) {
+                return ScaleTransition(
+                  scale: animation,
+                  child: ScaleTransition(scale: animation, child: child),
+                );
+              },
+              child: Icon(
+                icon,
+                size: 24,
+                key: ValueKey<bool>(isActive),
+                color: isActive
+                    ? AppColors.primaryColor
+                    : AppColors.inactiveBottomNavItemColor,
               ),
-              const SizedBox(height: 3),
-              Text(
-                title,
-                style: isActive == true
-                    ? AppStyles.secondaryMedium13
-                    : AppStyles.secondaryMedium13.copyWith(
-                        color: AppColors.inactiveBottomNavItemColor,
-                      ),
-              ),
-            ],
-          ),
+            ),
+            const SizedBox(height: 3),
+            Text(
+              title,
+              style: isActive == true
+                  ? AppStyles.secondaryMedium13
+                  : AppStyles.secondaryMedium13.copyWith(
+                      color: AppColors.inactiveBottomNavItemColor,
+                    ),
+            ),
+          ],
         ),
       ),
     );
