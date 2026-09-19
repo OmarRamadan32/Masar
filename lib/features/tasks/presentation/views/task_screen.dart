fa09202252055df203da0fb82d6ea5d71754d38c
Omar <omarramadan021@gmail.com>
2026-09-19 20:31:41 +0300

    feat: add button to mark task as incomplete in TaskScreen

--- lib/features/tasks/presentation/views/task_screen.dart
+++ lib/features/tasks/presentation/views/task_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:masar/core/theme/app_colors.dart';
 import 'package:masar/core/theme/app_sizes.dart';
 import 'package:masar/core/theme/app_styles.dart';
 import 'package:masar/core/widgets/custom_screen.dart';
@@ -22,6 +23,22 @@ class TaskScreen extends StatelessWidget {
           const TaskOverviewSectionTwo(),
           AppSizes.h20,
           Text("تفاصيل المهمة", style: AppStyles.primaryRegular16),
+          const Spacer(),
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              minimumSize: const Size(double.infinity, 50),
+              backgroundColor: AppColors.primaryColor,
+              shape: RoundedRectangleBorder(borderRadius: AppSizes.r16),
+            ),
+            child: Text(
+              "الإشارة كغير منجزة",
+              style: AppStyles.secondaryBold20.copyWith(
+                color: AppColors.surfacePrimaryColor,
+              ),
+            ),
+          ),
+          AppSizes.h20,
         ],
       ),
     );
