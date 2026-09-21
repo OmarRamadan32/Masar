4056a20fc387dbf877734287649250b54dd913ab
Omar <omarramadan021@gmail.com>
2026-09-21 21:38:00 +0300

    feat: add ScreenTitle widget to AddNoteScreen

--- lib/features/notes/presentation/views/add_note_screen.dart
+++ lib/features/notes/presentation/views/add_note_screen.dart
@@ -4,6 +4,7 @@ import 'package:masar/core/theme/app_sizes.dart';
 import 'package:masar/core/widgets/custom_screen.dart';
 import 'package:masar/core/widgets/custom_text_field.dart';
 import 'package:masar/core/widgets/opstions_picker.dart';
+import 'package:masar/features/main_layout/widgets/screen_title.dart';
 
 class AddNoteScreen extends StatelessWidget {
   const new({super.key});
@@ -18,6 +19,8 @@ class AddNoteScreen extends StatelessWidget {
       canPop: true,
       child: const Column(
         children: [
+          ScreenTitle(title: "اضافة ملاحظة", hasOptions: false),
+          AppSizes.h10,
           CustomTextField(isMultiLine: false, initialValue: "عنوان الملاحظة"),
           AppSizes.h10,
           OptionsPicker(),
